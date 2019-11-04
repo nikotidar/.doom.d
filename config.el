1198c9aef8602d0d0206aab55fe7aaa62071461a
Niko Tidar Lantang Perkasa <me@rndtx.id>
2019-11-04 15:49:40 +0700

    first commit

---
+++ config.el
@@ -0,0 +1,5 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq doom-font (font-spec :family "Iosevka" :size 13)
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 13))
