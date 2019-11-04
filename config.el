4196e2793cc0302ab15ac2caf6e880f088230825
Niko Tidar Lantang Perkasa <me@rndtx.id>
2019-11-04 16:02:38 +0700

    update

--- config.el
+++ config.el
@@ -1,5 +1,18 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
-(setq doom-font (font-spec :family "Iosevka" :size 13)
-      doom-variable-pitch-font (font-spec :family "Iosevka" :size 13))
+(setq user-full-name "Randall Tux"
+      user-mail-address "me@rndtx.id")
+
+;;; ui
+(setq doom-font (font-spec :family "Iosevka" :size 15)
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 15))
+
+;;; custom modeline
+(load! "lisp/modeline")
+
+
+(custom-theme-set-faces! 'doom-one-light
+  `(org-priority :background ,(doom-color 'bg))
+  `(mode-line :foreground ,(doom-color 'blue))
+  `(mode-line-buffer-id :foreground ,(doom-color 'fg))
+  `(mode0-line-success-highlight :background ,(doom-color 'green)))
